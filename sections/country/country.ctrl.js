76fe8e29226436b6696a113beca8317033a1d5f7
Sagar <sag.sahay@gmail.com>
2015-06-01 23:05:13 +0100

    trying to add neighbours

--- sections/country/country.ctrl.js
+++ sections/country/country.ctrl.js
@@ -7,8 +7,14 @@ angular.module('app').controller('country.ctrl', function (CountriesFactory,$loc
 
     vm.capitalPopulation= CountriesFactory.searchForCapitalPopulation(vm.country.countryCode,vm.country.countryName,vm.country.capital)
         .then(function(response){
-            console.log(response);
+            //console.log(response);
             return response.data.geonames[0].population;
+        });
+
+    vm.neighbours=CountriesFactory.getNeighbours(vm.country.geonameId)
+        .then(function(response){
+            console.log(response.data);
+            return response.data;
         });
 
     vm.goHome=function(){
--- services/country.fct.js
+++ services/country.fct.js
@@ -13,30 +13,30 @@ angular.module('app').factory('CountriesFactory', function ($http) {
                 method: 'JSONP',
                 url: url,
                 params: request,
-                cache:true
+                cache: true
             }).success(function (response) {
                 result = response;
             }).error(function (error) {
                 result = error;
             });
         },
-        "country":{} ,
+        "country": {},
 
-        'searchForCapitalPopulation':function(countryCode,name,q){
-            var url='http://api.geonames.org/searchJSON'
+        'searchForCapitalPopulation': function (countryCode, name, q) {
+            var url = 'http://api.geonames.org/searchJSON'
             var request = {
                 callback: "JSON_CALLBACK",
-                username:'sagar1986',
-                maxRows:2,
-                name:name,
-                countryCode:countryCode ,
-                q:q
+                username: 'sagar1986',
+                maxRows: 2,
+                name: name,
+                countryCode: countryCode,
+                q: q
 
             };
             return $http({
                 method: 'JSONP',
                 url: url,
-                params:request
+                params: request
 
             }).success(function (response) {
                 result = response;
@@ -44,6 +44,25 @@ angular.module('app').factory('CountriesFactory', function ($http) {
                 result = error;
             });
 
+        },
+
+        'getNeighbours': function (geonameId) {
+            var url = 'http://api.geonames.org/neighboursJSON';
+            var request = {
+                callback: "JSON_CALLBACK",
+                username: 'sagar1986',
+                geonameId: geonameId
+            };
+            return $http({
+                method: 'JSONP',
+                url: url,
+                params: request
+
+            }).success(function (response) {
+                result = response;
+            }).error(function (error) {
+                result = error;
+            });
         }
 
     }
